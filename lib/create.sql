bede4f4b871e178930bacae6b820fe6822bd3235
Kevin K <doilerjar@outlook.com>
2018-08-14 18:40:20 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -8,14 +8,14 @@ CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
-  funding_goal REAL,
+  funding_goal FLOAT,
   start_date DATE,
   end_date DATE
 );
 
 CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
-  amount REAL,
+  amount FLOAT,
   user_id INTEGER,
   project_id INTEGER
 );
