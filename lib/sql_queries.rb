05e229c6e90fdd2f15d8577742780ce4bbbffe19
Kevin K <doilerjar@outlook.com>
2018-08-14 18:21:38 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,10 +23,11 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, SUM(project.pledges)
+"SELECT projects.title, SUM(project.pledges)-projects.funding_goal
 FROM projects LEFT JOIN pledges
+ON projects.id = pledges.project_id
 GROUP BY(projects.title)
-WHERE SUM(projects.pledges) >= projects.
+WHERE SUM(projects.pledges) >= projects.funding_goal
 "
 end
 
