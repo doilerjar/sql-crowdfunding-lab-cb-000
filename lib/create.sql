5d41535394ec76631409d08435fd85d9a4fcb186
Kevin K <doilerjar@outlook.com>
2018-08-14 17:15:49 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,10 +1,11 @@
 CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
 )
 
 CREATE TABLE project (
-  id INTEGER PRIMARY,
+  id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
   funding_goal INTEGER,
