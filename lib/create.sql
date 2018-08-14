5fb93ba078b70a41b203099bb5c36b2e96f86a72
Kevin K <doilerjar@outlook.com>
2018-08-14 16:59:45 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+  name TEXT,
+  age INTEGER
+)
+
+CREATE TABLE project (
+  title TEXT,
+  category TEXT,
+  goal INTEGER,
+  start_date DATE,
+  end_date DATE
+)
+
+CREATE TABLE pledge (
+  amount 
+)
