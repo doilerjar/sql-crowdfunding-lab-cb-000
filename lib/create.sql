497d416351ca47063b2c857cbc3e7a14080f8431
Kevin K <doilerjar@outlook.com>
2018-08-14 17:26:57 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
-)
+);
 
 CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
@@ -11,11 +11,11 @@ CREATE TABLE projects (
   funding_goal REAL,
   start_date DATE,
   end_date DATE
-)
+);
 
 CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
   amount REAL,
   user_id INTEGER,
   project_id INTEGER
-)
+);
